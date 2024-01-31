ebf905fe0f3da3a59e99da3d9805eea915200e84
gillesashley <xylaray37@gmail.com>
2024-01-31 10:52:02 +0000

    configured django-jazzmin, created a CustomUser model, created a companies app to manage companies

--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local apps
     'pages.apps.PagesConfig',
+    'accounts.apps.AccountsConfig',
+    'companies.apps.CompaniesConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,10 +118,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+JAZZMIN_SETTINGS = {
+    'site_title': 'Teczaleel Admin',
+    'site_header': 'Teczaleel',
+    'site_brand': 'Teczaleel Admin',
+    'welcome_sign': 'Teczaleel',
+    'copyright': 'Teczaleel',
+}
