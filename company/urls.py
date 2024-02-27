a809c91092cb59c15ba3ed79e41ea53690b63c7f
gillesashley <xylaray37@gmail.com>
2024-02-27 18:39:30 +0000

    commented out query and context codes.

--- company/urls.py
+++ company/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import TrackProjectView, StartProjectView
+from .views import TrackProjectView
 
 app_name = 'company'
 urlpatterns = [
     path('track-project/', TrackProjectView.as_view(), name='track-project'),
-    path('start-project/', StartProjectView.as_view(), name='start-project'),
 ]
